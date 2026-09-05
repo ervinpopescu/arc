5ba53ba5bba9b831eee0bf0f03337a108874fba4
ervinpopescu <ervin.popescu10@gmail.com>
2026-09-05 19:20:20 +0300

    feat(runners): add job-applier runner scale set configuration

---
+++ runners/job-applier/defaults.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export DEFAULT_ARC_INSTALLATION_NAME="arc"
+export DEFAULT_ARC_NAMESPACE="arc-systems"
+export DEFAULT_RUNNERSET_INSTALLATION_NAME="job-applier"
+export DEFAULT_RUNNERS_NAMESPACE="job-applier-runners"
+export DEFAULT_SECRET_NAME="pre-defined-secret"
+export DEFAULT_OVERRIDES_PATH="./runners/job-applier/values.runner-set.yaml"
+export DEFAULT_MINIKUBE_PROFILE="prod"
+export MINIKUBE_DRIVER="kvm2"
+export MIN_NODES=2
+export MINIKUBE_CNI=calico
